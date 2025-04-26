e2ff037c28a2e2563642a5f0808816c540848d58
RacquelNinaDennison <racquelnina@gmail.com>
2025-04-26 15:50:36 +0200

    Chnaged file name

--- src/site.config.ts
+++ src/site.config.ts
@@ -27,51 +27,56 @@ export const siteConfig: SiteConfig = {
 	*/
 	title: "Racquel Dennison",
 	// ! Please remember to replace the following site property with your own domain, used in astro.config.ts
-	url: "https://astro-cactus.chriswilliams.dev/",
+	url: "https://racquelninadennisonpersonalwebsite.vercel.app/",
 };
 
 // Used to generate links in both the Header & Footer.
-export const menuLinks: { path: string; title: string }[] = [
-	{
-		path: "/",
-		title: "Home",
-	},
-	{
-		path: "/about/",
-		title: "About",
-	},
-	{
-		path: "/blogs/",
-		title: "Blogs",
-	},
-];
+export const menuLinks: { path: string; title: string }[] =
+	[
+		{
+			path: "/",
+			title: "Home",
+		},
+		{
+			path: "/about/",
+			title: "About",
+		},
+		{
+			path: "/blogs/",
+			title: "Blogs",
+		},
+	];
 
 // https://expressive-code.com/reference/configuration/
-export const expressiveCodeOptions: AstroExpressiveCodeOptions = {
-	styleOverrides: {
-		borderRadius: "4px",
-		codeFontFamily:
-			'ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
-		codeFontSize: "0.875rem",
-		codeLineHeight: "1.7142857rem",
-		codePaddingInline: "1rem",
-		frames: {
-			frameBoxShadowCssValue: "none",
+export const expressiveCodeOptions: AstroExpressiveCodeOptions =
+	{
+		styleOverrides: {
+			borderRadius: "4px",
+			codeFontFamily:
+				'ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
+			codeFontSize: "0.875rem",
+			codeLineHeight: "1.7142857rem",
+			codePaddingInline: "1rem",
+			frames: {
+				frameBoxShadowCssValue: "none",
+			},
+			uiLineHeight: "inherit",
 		},
-		uiLineHeight: "inherit",
-	},
-	themeCssSelector(theme, { styleVariants }) {
-		// If one dark and one light theme are available
-		// generate theme CSS selectors compatible with cactus-theme dark mode switch
-		if (styleVariants.length >= 2) {
-			const baseTheme = styleVariants[0]?.theme;
-			const altTheme = styleVariants.find((v) => v.theme.type !== baseTheme?.type)?.theme;
-			if (theme === baseTheme || theme === altTheme) return `[data-theme='${theme.type}']`;
-		}
-		// return default selector
-		return `[data-theme="${theme.name}"]`;
-	},
-	// One dark, one light theme => https://expressive-code.com/guides/themes/#available-themes
-	themes: ["dracula", "github-light"],
-	useThemedScrollbars: false,
-};
+		themeCssSelector(theme, { styleVariants }) {
+			// If one dark and one light theme are available
+			// generate theme CSS selectors compatible with cactus-theme dark mode switch
+			if (styleVariants.length >= 2) {
+				const baseTheme = styleVariants[0]?.theme;
+				const altTheme = styleVariants.find(
+					(v) => v.theme.type !== baseTheme?.type
+				)?.theme;
+				if (theme === baseTheme || theme === altTheme)
+					return `[data-theme='${theme.type}']`;
+			}
+			// return default selector
+			return `[data-theme="${theme.name}"]`;
+		},
+		// One dark, one light theme => https://expressive-code.com/guides/themes/#available-themes
+		themes: ["dracula", "github-light"],
+		useThemedScrollbars: false,
+	};
